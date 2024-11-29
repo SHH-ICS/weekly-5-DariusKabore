c0bc70acbc874b7f1f69fa8eec35ab95d1211808
Darius Kabore <darius.kabore@stu.ocsb.ca>
2024-11-29 15:48:16 +0000

    done

--- handler.php
+++ handler.php
@@ -1,23 +1,39 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Welcome Page</title>
+</head>
+
+<body>
+
+  <?php
+  if (isset($_POST["iterations"])) {
+    $iterations = $_POST["iterations"];
+
+    if (is_numeric($iterations) && $iterations > 0) {
+      $iterations = (int)$iterations; // Ensure it's an integer
+      $pi = 0;
+
+      for ($i = 0; $i < $iterations; $i++) {
+        $pi += pow(-1, $i) / (2 * $i + 1);
+      }
+      $pi *= 4;
+
+      echo "<h2>Approximated value of PI after $iterations iterations: $pi</h2>";
+    } else {
+      // Display error message for invalid input
+      echo "<h2 style='color: red;'>Please enter a valid positive integer for the number of iterations.</h2>";
     }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+  }
+  ?>
+
+
+
+
+</body>
+
 </html>
